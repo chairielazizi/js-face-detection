00903724c47bf73d8e1c1ebf86c8d244c20c2577
chairielazizi <azizihr2000@gmail.com>
2020-11-23 14:28:04 +0800

    console log  the value if faceapi

--- script.js
+++ script.js
@@ -19,4 +19,8 @@ function startVideo(){
 
 video.addEventListener('play', () => {
     console.log('its a face!')
+    setInterval(async () =>{
+        const detections = await faceapi.detectAllFaces(video,new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        console.log(detections)
+    }, 100)
 })
